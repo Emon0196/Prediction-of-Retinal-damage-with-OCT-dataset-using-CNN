45bc0b3180822c5297cefd4a6706bcefb91919a6
Mehedi Hasan Emon <111483791+Emon0196@users.noreply.github.com>
2024-03-04 01:30:08 +0600

    Delete admin directory

--- admin/delete_hotel.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once 'connect.php';
-	$conn->query("DELETE FROM `hotel` WHERE `h_id` = '$_REQUEST[h_id]'") or die(mysql_error());
-	header("location:hotel.php");
-?>
--- admin/delete_room.php
+++
@@ -1,5 +0,0 @@
-<?php
-	require_once 'connect.php';
-	$conn->query("DELETE FROM `room` WHERE `room_id` = '$_REQUEST[room_id]'") or die(mysql_error());
-	header("location:room.php");
-?>
